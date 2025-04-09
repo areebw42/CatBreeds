1c39b4fc966d0882c6fb41ee14f40b00e43f6083
Areeb Durrani <areebw42@gmail.com>
2025-04-09 15:33:41 -0500

    Initial Commit

---
+++ CatBreeds/CatBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatBreedsApp.swift
+//  CatBreeds
+//
+//  Created by Areeb Durrani on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CatBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
